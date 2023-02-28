741e3ce9159394a8fb13c82779bfc60500577671
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-02-28 15:01:09 -0500

    adds new keys to the Schema

--- backend/models/petFinder.js
+++ backend/models/petFinder.js
@@ -5,10 +5,15 @@ const PetSchema = new mongoose.Schema({
 	shelterId: String,
 	name: String,
 	photo: String,
+    description:String,
 	age: String,
 	animal: String,
 	breed: String,
-	sex: String
+	sex: String,
+    city: String,
+    state: String,
+    postcode: String,
+    status: String
 })
 
 module.exports = mongoose.model("petFinder", PetSchema);
